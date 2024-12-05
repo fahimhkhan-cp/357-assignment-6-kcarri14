9767ec7c0f330c23eb9ee95fc3e1c4a6688e1b33
kcarri14 <kcarri14@calpoly.edu>
2024-12-05 14:39:44 -0800

    try again

--- corgis.h
+++ corgis.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 typedef struct{
     char county[100];                                    // Name of the county
@@ -60,6 +61,20 @@ typedef struct{
     uint64_t Employment_Firms_Women_Owned;
 } Info;
 
+void trim_whitespace(char *str) {
+    char *end;
+
+    // Trim leading space
+    while (isspace((unsigned char)*str)) str++;
+
+    // Trim trailing space
+    end = str + strlen(str) - 1;
+    while (end > str && isspace((unsigned char)*end)) end--;
+
+    // Write new null terminator
+    *(end + 1) = '\0';
+}
+
 int parsing(char *line, Info *information){
     char buffer[1024];
     strcpy(buffer, line);
@@ -68,213 +83,265 @@ int parsing(char *line, Info *information){
     if(!token){
         return -1;
     }
+    trim_whitespace(token);
     strcpy(information->county, token);
     //copy state into structure
     token = strtok(NULL, ",");
     if(!token){
         return -1;
     }
+    trim_whitespace(token);
     strcpy(information->state, token);
 
     token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Age_Percent_65_and_Older = atof(token);
 
     token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Age_Percent_Under_18_Years = atof(token);
 
     token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Age_Percent_Under_5_Years = atof(token);
 
     token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Education_Bachelors_Degree_or_Higher = atof(token);
 
     token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Education_High_School_or_Higher = atof(token);
 
     token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Employment_Nonemployer_Establishments = atoll(token);
 
     token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Employment_Private_Non_farm_Employment = atoll(token);
 
     token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Employment_Private_Non_farm_Employment_Change = atof(token);
 
     token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Employment_Private_Non_farm_Establishments = atoll(token);
 
     token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Ethnicities_American_Indian_and_Alaska_Native_Alone = atof(token);
 
     token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Ethnicities_Asian_Alone = atof(token);
 
     token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Ethnicities_Black_Alone = atof(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Ethnicities_Hispanic_or_Latino = atof(token);
 
     token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Ethnicities_Native_Hawaiian_and_Other_Pacific_Islander_Alone = atof(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Ethnicities_Two_or_More_Races   = atof(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Ethnicities_White_Alone = atof(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Ethnicities_White_Alone_not_Hispanic_or_Latino = atof(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Housing_Homeownership_Rate = atof(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Housing_Homeownership_Rate = atoll(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Housing_Housing_Units = atoll(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Housing_Median_Value_of_Owner_Occupied_Units = atoll(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Housing_Persons_per_Household = atof(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Housing_Units_in_Multi_Unit_Structures = atof(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Income_Median_Household_Income = atoll(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Income_Per_Capita_Income = atoll(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Income_Persons_Below_Poverty_Level = atof(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Miscellaneous_Building_Permits = atoll(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Miscellaneous_Foreign_Born = atof(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Miscellaneous_Land_Area = atof(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Miscellaneous_Language_Other_than_English_at_Home = atof(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Miscellaneous_Living_in_Same_House_1_Years = atof(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Miscellaneous_Manufacturers_Shipments = atoll(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Miscellaneous_Mean_Travel_Time_to_Work = atof(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Miscellaneous_Percent_Female = atof(token);
 
     token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Miscellaneous_Veterans = atoll(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Population_2010_Population = atoll(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Population_2014_Population = atoll(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Population_Population_Percent_Change = atof(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Population_Population_per_Square_Mile = atof(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Sales_Accommodation_and_Food_Services_Sales = atoll(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Sales_Merchant_Wholesaler_Sales = atoll(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Sales_Retail_Sales = atoll(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Sales_Retail_Sales_per_Capita = atoll(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Employment_Firms_American_Indian_Owned = atoll(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Employment_Firms_Asian_Owned = atoll(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Employment_Firms_Black_Owned = atoll(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
+    trim_whitespace(token);
     information->Employment_Firms_Hispanic_Owned = atoll(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
-    information->Employment_Firms_Native_Hawaiian_Owned = atof(token);
+    trim_whitespace(token);
+    information->Employment_Firms_Native_Hawaiian_Owned = atoll(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
-    information->Employment_Firms_Total = atof(token);
+    trim_whitespace(token);
+    information->Employment_Firms_Total = atoll(token);
 
      token = strtok(NULL, ",");
     if (!token) return -1;
-    information->Employment_Firms_Women_Owned = atof(token);
+    trim_whitespace(token);
+    information->Employment_Firms_Women_Owned = atoll(token);
     
     return 0;
  }
