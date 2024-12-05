df77f3170e61ff9f0a5f942b5cb969038be35d1a
kcarri14 <kcarri14@calpoly.edu>
2024-12-05 14:41:58 -0800

    lets try this

--- corgis.h
+++ corgis.h
@@ -62,17 +62,11 @@ typedef struct{
 } Info;
 
 void trim_whitespace(char *str) {
-    char *end;
-
-    // Trim leading space
-    while (isspace((unsigned char)*str)) str++;
-
-    // Trim trailing space
-    end = str + strlen(str) - 1;
-    while (end > str && isspace((unsigned char)*end)) end--;
-
-    // Write new null terminator
-    *(end + 1) = '\0';
+    int len = strlen(str);
+    if (len > 1 && str[0] == '"' && str[len - 1] == '"') {
+        memmove(str, str + 1, len - 2);
+        str[len - 2] = '\0';
+    }
 }
 
 int parsing(char *line, Info *information){
