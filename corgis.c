62d27d9afcdcf65351db3fc914b3919b8a1496b5
kcarri14 <kcarri14@calpoly.edu>
2024-12-05 17:12:13 -0800

    again

--- corgis.c
+++ corgis.c
@@ -111,7 +111,6 @@ void filter_field(Info *information, int *count, char *field, char *comparison, 
     }
 
     *count = new_count;
-    printf("%d\n", *count);
     printf("Filter: %s, %s, %.2f (%d entries)\n", field, comparison, value, *count);
 
 }
@@ -223,7 +222,6 @@ int execute_operation(char *line, Info *information, int *count){
         char *field_wanted = strtok(NULL, "\n");
         population_field(information, count, field_wanted);
     }else if(strcmp(function_wanted, "display") == 0){
-        printf("%d\n", *count);
         display_counties(information, count);
     }else{
         fprintf(stderr, "malformed line");
